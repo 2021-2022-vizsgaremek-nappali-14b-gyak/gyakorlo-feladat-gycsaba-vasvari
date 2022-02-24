130509402599b221bf4ebdcd267aace2cdc7e988
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-24 08:43:32 +0100

    A ViewModelt összekötjük a repoisotry réteggel. Példányosítunk egy repo változót.

--- Kreta/ViewModel/ClassViewModel.cs
+++ Kreta/ViewModel/ClassViewModel.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Kreta.Models;
+using Kreta.Repositories;
+
 namespace Kreta.ViewModel
 {
     class ClassViewModel
     {
+        private ClassesRepo classesRepo;
+
+        public ClassViewModel()
+        {
+            classesRepo = new ClassesRepo();
+        }
+
     }
 }
