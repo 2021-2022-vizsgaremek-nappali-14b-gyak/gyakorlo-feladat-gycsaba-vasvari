7b5747e007f0a963e4344896d18ac453b637b714
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-24 08:45:34 +0100

    Classes peoperty a View-n való megjelenéshez

--- Kreta/ViewModel/ClassViewModel.cs
+++ Kreta/ViewModel/ClassViewModel.cs
@@ -6,17 +6,20 @@ using System.Threading.Tasks;
 
 using Kreta.Models;
 using Kreta.Repositories;
+using System.Collections.ObjectModel;
 
 namespace Kreta.ViewModel
 {
     class ClassViewModel
     {
         private ClassesRepo classesRepo;
+        private ObservableCollection<Class> classes;
 
         public ClassViewModel()
         {
             classesRepo = new ClassesRepo();
         }
 
+        public ObservableCollection<Class> Classes { get => classes; set => classes = value; }
     }
 }
