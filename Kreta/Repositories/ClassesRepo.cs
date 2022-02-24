07402a61cc145c78b5a00d5afa561d2a427302f3
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-24 08:29:39 +0100

    ClassRepo osztály és classes lista az osztályok tárolására

---
+++ Kreta/Repositories/ClassesRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kreta.Models;
+
+namespace Kreta.Repositories
+{
+    public class ClassesRepo
+    {
+        private List<Class> classes;
+
+        public ClassesRepo()
+        {
+            classes = new List<Class>();
+        }
+    }
+}
