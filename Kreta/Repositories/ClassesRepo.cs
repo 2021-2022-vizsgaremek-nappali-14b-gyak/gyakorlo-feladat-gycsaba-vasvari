0df6782f68d0bd27a2e47e2f79ca70453f434bff
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-24 08:33:44 +0100

    MakeTestData metódus kész

--- Kreta/Repositories/ClassesRepo.cs
+++ Kreta/Repositories/ClassesRepo.cs
@@ -15,6 +15,15 @@ namespace Kreta.Repositories
         public ClassesRepo()
         {
             classes = new List<Class>();
+            MakeTestData();
+        }
+
+        public void MakeTestData()
+        {
+            classes.Add(new Class(1, 9, 'a', 4));
+            classes.Add(new Class(2, 9, 'b', 1));
+            classes.Add(new Class(3, 10, 'a', 6));
+            classes.Add(new Class(4, 10, 'b', 3));
         }
     }
 }
